f05f8e6c3c2690f2df42272ed7956db90470d4e0
applenele <1941973283@qq.com>
2015-08-22 22:35:30 +0800

    volley

--- SmartProtecter/app/src/main/java/com/newren/smartprotecter/activity/LoginActivity.java
+++ SmartProtecter/app/src/main/java/com/newren/smartprotecter/activity/LoginActivity.java
@@ -5,16 +5,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.newren.smartprotecter.R;
 
 public class LoginActivity extends Activity {
 
-
+   private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.signin_button);
+
+        btnLogin.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                String strTmp = "µã»÷Button01";
+            }
+        });
+
     }
 
 
