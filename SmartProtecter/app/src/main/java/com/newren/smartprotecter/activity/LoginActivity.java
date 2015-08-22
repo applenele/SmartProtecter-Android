f26977d1432ef564b32ac13ebb840c167db6380f
applenele <1941973283@qq.com>
2015-08-22 18:04:01 +0800

    login

--- SmartProtecter/app/src/main/java/com/newren/smartprotecter/activity/LoginActivity.java
+++ SmartProtecter/app/src/main/java/com/newren/smartprotecter/activity/LoginActivity.java
@@ -1,19 +1,21 @@
 package com.newren.smartprotecter.activity;
 
 import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.newren.smartprotecter.R;
 
+public class LoginActivity extends Activity {
 
-public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
 
 }
