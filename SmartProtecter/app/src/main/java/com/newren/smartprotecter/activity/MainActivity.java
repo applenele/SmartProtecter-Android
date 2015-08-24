2e9755d1eb82077ba4b8bfae258a24828713c5bc
applenele <1941973283@qq.com>
2015-08-24 10:23:32 +0800

    切换

--- SmartProtecter/app/src/main/java/com/newren/smartprotecter/activity/MainActivity.java
+++ SmartProtecter/app/src/main/java/com/newren/smartprotecter/activity/MainActivity.java
@@ -2,18 +2,69 @@ package com.newren.smartprotecter.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
 
 import com.newren.smartprotecter.R;
+import com.newren.smartprotecter.fragment.FragmentAccident;
+import com.newren.smartprotecter.fragment.FragmentMe;
+import com.newren.smartprotecter.fragment.FragmentPublishment;
+import com.newren.smartprotecter.fragment.FragmentSetting;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    Fragment accident = null;
+    Fragment publishment = null;
+    Fragment me = null;
+    Fragment setting = null;
+    RadioGroup myTabRg = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    public void initView() {
+        accident = new FragmentAccident();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, accident).commit();
+        myTabRg = (RadioGroup) findViewById(R.id.tab_menu);
+        myTabRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbAccident:
+                        accident = new FragmentAccident();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, accident)
+                                .commit();
+                        break;
+                    case R.id.rbPublish:
+                        if (publishment == null) {
+                            publishment = new FragmentPublishment();
+                        }
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, publishment).commit();
+                        break;
+                    case R.id.rbMe:
+                        me = new FragmentMe();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, me)
+                                .commit();
+                        break;
+                    case R.id.rbSetting:
+                        setting = new FragmentSetting();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, setting)
+                                .commit();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        });
     }
 
 }
---
+++ SmartProtecter/app/src/main/java/com/newren/smartprotecter/fragment/FragmentMe.java
@@ -0,0 +1,28 @@
+package com.newren.smartprotecter.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.newren.smartprotecter.R;
+
+/**
+ * Created by ¿÷ on 2015/8/24.
+ */
+public class FragmentMe extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return inflater.inflate(R.layout.me, null);
+    }
+}
