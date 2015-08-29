ad3e6a83742d30f76ac567a52050bf8e3e7c8577
applenele <1941973283@qq.com>
2015-08-29 16:19:43 +0800

    1

--- SmartProtecter/app/src/main/java/com/newren/smartprotecter/fragment/FragmentAccidentShow.java
+++ SmartProtecter/app/src/main/java/com/newren/smartprotecter/fragment/FragmentAccidentShow.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,12 +20,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.newren.smartprotecter.R;
 import com.newren.smartprotecter.model.Accident;
+import com.newren.smartprotecter.model.User;
 import com.newren.smartprotecter.util.QueueApplication;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,7 +46,7 @@ public class FragmentAccidentShow extends Fragment {
     private  TextView tvDescription = null;
     private TextView tvAId = null;
     private Button btnSub = null;
-    private TextView tvReply = null;
+    private EditText tvReply = null;
     private Handler handler=new Handler(){
         public void handleMessage(Message msg){
             Toast toast= Toast.makeText(getActivity(), msg.obj.toString(), Toast.LENGTH_SHORT);
@@ -60,7 +64,7 @@ public class FragmentAccidentShow extends Fragment {
         tvDescription = (TextView) myView.findViewById(R.id.txtDescription);
         tvAId = (TextView) myView.findViewById(R.id.aid);
         btnSub = (Button) myView.findViewById(R.id.btnSub);
-        tvReply = (TextView) myView.findViewById(R.id.txtReply);
+        tvReply = (EditText) myView.findViewById(R.id.txtReply);
         String i = getArguments().getString("key");
         Log.i("id", i);
         String url = "http://121.42.136.4:9000/AccidentApi/GetAccident?id="+i;
@@ -118,8 +122,51 @@ public class FragmentAccidentShow extends Fragment {
         btnSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String content = (String) tvReply.getText();
-                Log.i("content",content);
+                String content = tvReply.getText().toString();
+                String aid = (String) tvAId.getText();
+                User user= QueueApplication.getUser();
+                String params ="";
+                params = "content="+ URLEncoder.encode(content)+"&aid="+aid+"&uid="+user.getId();
+
+                String publishmenturl = "http://121.42.136.4:9000/AccidentApi/Reply?"+params;
+
+                JsonObjectRequest publishmentrequest = new JsonObjectRequest(publishmenturl,null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String msg = response.getString("Msg");
+                                    String statu = response.getString("Statu");
+                                    if(statu.equals("ok")){
+                                        MsgThread msgThread = new MsgThread("提交成功！");
+                                        new Thread(msgThread).start();
+                                    }else{
+                                        MsgThread msgThread = new MsgThread(msg);
+                                        new Thread(msgThread).start();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                MsgThread msgThread = new MsgThread("出现错误");
+                                new Thread(msgThread).start();
+                            }
+                        }
+                ) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("Accept", "application/json");
+                        headers.put("Content-Type", "application/json; charset=UTF-8");
+                        return headers;
+                    }
+                };
+
+                QueueApplication.getHttpQueues().add(publishmentrequest);
             }
         });
 
